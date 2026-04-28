8fef11a6241d319ff9c437fecc79ccf05e18f130
sarvanan-prog <saravanan.mca1992@gmail.com>
2026-04-28 09:14:41 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const title = <h1 className="title"> Hello welcome to React Js world </h1>;
-const author = <h4 className="author"> Developer Name : Saravanan </h4>;
-const fruitArray = ["apple", "orange", "grapes", "banana"];
-const isFruitAvailable = true;
-const vegtableListstatus = true;
+
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
