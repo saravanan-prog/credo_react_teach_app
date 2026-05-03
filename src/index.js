5660182f3362e58b3a0f36006de4a0e7793d74d8
sarvanan-prog <saravanan.mca1992@gmail.com>
2026-05-04 00:48:09 +0530

    index workout

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import NameList from './Pages/b_inputButtonEvent/NameList';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <NameList />
+    
   </React.StrictMode>
 );
 
