7f21a5b2334b45c8f6b72f6ff0c47a43b5c5b278
sarvanan-prog <saravanan.mca1992@gmail.com>
2026-05-09 12:17:09 +0530

    code changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Student from "./Pages/b_props_drilling/Student";
-import Counter from "./Pages/c_props_callback/Counter";
+
+
+
+
 
 
 
@@ -10,6 +12,6 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <Counter />
+    
   </React.StrictMode>,
 );
