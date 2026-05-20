987004d0e0762bf9ac2787115b4f32682ef564cc
sarvanan-prog <saravanan.mca1992@gmail.com>
2026-05-20 11:14:33 +0530

    state

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,20 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import CustomerList from './CustomerList'
 
 export default function Homepage(){
 
-    const canidatename = "Kirubha"
-
-    const handleClick = (data) => {
-        console.log("button is clicked ",data)
-    }
+    const [candidateName ,setCandidateName] = useState("saravanan")
+    const [skillset,setSkillset] = useState(["React","Angular","node","vuejs"])
+    const [experinece,setExperience] = useState(
+        {
+            "2022":"Tcs",
+            "2023" : "CTS",
+            "2024" : "Techmanindra"
+        }
+    )
+   
 
     return <div>
-         <h2> Parent Component  </h2>
-
-         <CustomerList 
-            canidatename = {canidatename}
-            handleClick = {handleClick}
-         />
+         <h2> Home Page  </h2>
+        <p> Canidate Name : {candidateName} </p>
+        <h3> Skill set </h3>
+        {skillset.map((value,index)=> <p> {value} </p>)}
     </div>
 }
