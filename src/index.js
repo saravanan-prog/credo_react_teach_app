1f3e3173eb4a6857099dde96fda861ea9c54d318
sarvanan-prog <saravanan.mca1992@gmail.com>
2026-05-24 10:41:08 +0530

    index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import UncontrolledPage from "./pages/a_useRef-hook/UncontrolledPage.jsx";
-import UnControlledForm from "./pages/a_useRef-hook/UncontrolledForm.js";
-import Counter from "./pages/b_usememo-hook/Counter.jsx";
-import Calculator from "./pages/c_useCallback-hook/Calculator.jsx";
-import DemoPage from "./pages/d_demo-page/DemoPage.jsx";
+
 
 
 
@@ -16,6 +12,6 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-     <DemoPage />
+     
   </React.StrictMode>
 );
