0fb62ea93dd69fe58d5dccb37dfc3a37e7c8fad2
sarvanan-prog <saravanan.mca1992@gmail.com>
2026-06-07 18:44:25 +0530

    routing-page

--- src/router/RoutingOutlet.jsx
+++ src/router/RoutingOutlet.jsx
@@ -4,19 +4,12 @@ import AboutPage from '../Pages/about/Aboutpage'
 import BlogPage from '../Pages/blog/BlogPage'
 import ContactPage from "../Pages/contact/ContactPage"
 import DashboardPage from "../Pages/dashboard/DashboardPage"
-import ProductsPage from "../Pages/products/ProductsPage"
 import PageNotFound from "../Pages/pagenotFound/PageNotFound"
 import Header from "../core/resusble-components/Header"
-import ViewFullProduct from "../Pages/products/view-product/ViewFullProduct"
-import ButtonHeader from "../core/resusble-components/ButtonHeader"
 import LoginForm from "../Pages/Login/LoginForm"
-import ProtectionRoute from "./ProtectionRoute"
-import DynamicRoutePage from "../Pages/paramter-page/DynamicRoutePage"
-import LocationRoutePage from "../Pages/paramter-page/LocationRoutePage"
 
 export default function RoutingOutlet(){
 
-    
 
     return <div>
         <BrowserRouter>
@@ -28,29 +21,7 @@ export default function RoutingOutlet(){
                 <Route path="blogs" element= { <BlogPage /> }  />
                 <Route path="contact" element= { <ContactPage /> } />  
                 <Route path ="login" element = {<LoginForm />} />
-
-                <Route path="dashboard" element= { 
-                    <ProtectionRoute>
-                         <DashboardPage /> 
-                    </ProtectionRoute>
-                   
-
-                    }  
-                />
-
-                <Route path="products" element= { <ProductsPage /> }   />
-                <Route path = "viewproduct" element = {<ViewFullProduct/>} />
-                <Route path = "viewproduct/:id" element = {<ViewFullProduct/>} />
-                <Route path = "viewproduct/:id/:name" element = {<ViewFullProduct/>} />.
-
-
-
-                 <Route path="dynamic" element= { <DynamicRoutePage /> } />
-                 <Route path="dynamic/:id" element= { <DynamicRoutePage /> } />
-                 <Route path="dynamic/:id/:name" element= { <DynamicRoutePage /> } />
-
-                 <Route path="location" element= { <LocationRoutePage /> } />
-
+                <Route path ="dashboard" element = {<DashboardPage />} />
                 <Route path="*" element={<PageNotFound />} />
             </Routes>
             
