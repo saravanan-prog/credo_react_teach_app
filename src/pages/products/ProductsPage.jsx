302c127413651d7c30a915b3c5926655b369ed56
saravanan-prog <saravanan.mca1992@gmail.com>
2026-06-15 11:28:57 +0530

    coding products

--- src/pages/products/ProductsPage.jsx
+++ src/pages/products/ProductsPage.jsx
@@ -4,23 +4,26 @@ import { productsList } from "./api-thunk"
 
 export default function ProductPage(){
   
-    const loading = useSelector((state)=> state?.product?.loading)
-    const error = useSelector((state)=> state?.product?.error)
-    const productList = useSelector((state)=> state?.product?.data)
-    useSelector((state)=> console.log(state))
-
-    console.log("productList===>",productList)
+    
 
     const dispatch = useDispatch()
     useEffect(()=>{
         dispatch(productsList())
     },[])
+    
+    useSelector((state)=> console.log("State====>",state))
+    const loading = useSelector((state)=> state?.products?.loading)
+    const error = useSelector((state)=> state?.products?.error)
+    const productList = useSelector((state)=> state?.products?.data)
+
+
+    console.log("productList===>",productList)
 
     
     return <div>
         <h1> Product Page </h1>
         {
-            loading && <h1> Loading </h1>
+            loading && <h1> Loading... </h1>
         }
         {error  && <h1> error </h1>
         }
