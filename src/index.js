0a8814863d6d56d322bfbeaf4d77eece936dc577
saravanan-prog <saravanan.mca1992@gmail.com>
2026-08-18 10:31:08 +0530

    component life cycle

--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import D_UnmountPhase from "./pages/D_UnmountPhase";
+
+
+function MainPage(){
+
+  const [disable,setDisable] = useState(false)
+
+
+  return (
+    <>
+       {!disable &&  <D_UnmountPhase /> }
+       <button onClick ={()=>setDisable(true)} > Disable </button>
+    
+    </>
+  )
+}
+
+
+
+
+
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
   
-    <D_UnmountPhase />
+    <MainPage />
 
-  </React.StrictMode>,
+  // </React.StrictMode>,
 );
