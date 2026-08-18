3d24ef3b9c9553059bfafe5419b92581d77788ab
saravanan-prog <saravanan.mca1992@gmail.com>
2026-08-18 08:35:27 +0530

    component

---
+++ src/pages/B_props-drilling/ComponentA.jsx
@@ -0,0 +1,14 @@
+export default function ComponentA(){
+
+
+
+    return(
+        <>
+          <div className="title"><h1> React Props </h1></div>
+
+
+        
+        
+        </>
+    )
+}
---
+++ src/pages/C_functional-props/ComponentB.jsx
@@ -0,0 +1,14 @@
+export default function ComponentB(){
+
+
+
+    return(
+        <>
+          <div className="title"><h1> React Props </h1></div>
+
+
+        
+        
+        </>
+    )
+}
