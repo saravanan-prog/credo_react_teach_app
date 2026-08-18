a2cb4aa81195612f8a731bcc0ee638eb0f9682e4
saravanan-prog <saravanan.mca1992@gmail.com>
2026-08-18 08:52:11 +0530

    code-changes

---
+++ src/pages/c_functional-props/AparentComponent.jsx
@@ -0,0 +1,14 @@
+export default function AparentComponent(){
+
+
+
+    return(
+        <>
+          <div className="title"><h1> Functional Props </h1></div>
+
+
+        
+        
+        </>
+    )
+}
---
+++ src/pages/f_callback/ComponentA.jsx
@@ -0,0 +1,14 @@
+export default function ComponentA(){
+
+
+
+    return(
+        <>
+          <div className="title"><h1> React Props Callback </h1></div>
+
+
+        
+        
+        </>
+    )
+}
