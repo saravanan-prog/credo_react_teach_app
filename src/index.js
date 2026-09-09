c7c7582164043d3f918959b776ebcf8ebe140cf2
saravanan-prog <saravanan.mca1992@gmail.com>
2026-09-10 01:22:17 +0530

    React toolkit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import CounterPage from "./pages/counter/CounterPage";
 
-import { Provider } from "react-redux";
-import { store } from "./store/store";
 
 
 
@@ -12,10 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
 
-    <Provider store ={store}>
-      <CounterPage />  
-    </Provider>
-   
+
     
     
   </React.StrictMode>
